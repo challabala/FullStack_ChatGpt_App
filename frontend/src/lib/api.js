63266de904de7ccde8bbce95b260a112781b0093
challabala <challabala58@gmail.com>
2026-01-06 10:53:28 +0530

    backend url added

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000"||"https://fullstack-chatgpt-app.onrender.com",
+  baseURL: "https://fullstack-chatgpt-app.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
